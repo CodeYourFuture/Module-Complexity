2c0092ab1939ff044a554e65c6362ac63775b7d8
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-06-09 22:31:30 +0100

    Optimize findCommonItems by replacing includes() with Set lookup

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,14 +1,28 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity:O(n * m)
+ * Space Complexity: O(k) k<=n
+ * Optimal Time Complexity: O(n+m+k)
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
+/*export const findCommonItems = (firstArray, secondArray) => [
   ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+];*/
+
+export const findCommonItems = (firstArray, secondArray) => {
+  const setSecond = new Set(secondArray); // O(m)
+  const seen = new Set();
+
+  for (const item of firstArray) { // )(n)
+    if (setSecond.has(item)) {
+      seen.add(item);
+    }
+  }
+
+  return [...seen]; //)(k)
+};
+
