9b1e49479b15e16b9c5653c085b59eae68d1bea9
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-06-10 11:27:59 +0100

    optimise removeDuplicates to reduce time complexity

--- Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
+++ Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
@@ -1,14 +1,13 @@
 /**
  * Remove duplicate values from a sequence, preserving the order of the first occurrence of each value.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity: O(n^2) because we have two nested loops
+ * Space Complexity: O(n) uniqueItems array stores up to n elements if all are unique
  *
  * @param {Array} inputSequence - Sequence to remove duplicates from
  * @returns {Array} New sequence with duplicates removed
  */
-export function removeDuplicates(inputSequence) {
+/*export function removeDuplicates(inputSequence) {
   const uniqueItems = [];
 
   for (
@@ -33,4 +32,21 @@ export function removeDuplicates(inputSequence) {
   }
 
   return uniqueItems;
+}*/
+
+//Optimal Time Complexity: O(n) we iterate through the inputSequence once
+
+export function removeDuplicates(inputSequence) {
+  const seen = new Set();
+  const uniqueItems = [];
+
+  for (const item of inputSequence) {
+    if (!seen.has(item)) {
+      seen.add(item);
+      uniqueItems.push(item);
+    }
+  }
+
+  return uniqueItems;
 }
+
