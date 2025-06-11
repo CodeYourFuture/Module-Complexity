fea06108ec3277b75e4d39794a86c58b91106df9
ameneh-keshavarz <amy.cyf2024@gmail.com>
2025-06-11 22:15:30 +0100

    correct explantion of findCommonItems

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,8 +1,8 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:O(n * m) because for each element in firstArray, the includes method does a linear search which can take up to m steps, 
- * so that would be O(n*m)
+ * Time Complexity:O(n * m) because for each element in firstArray, the includes method does a linear search in the second array
+ *  which can take up to m steps, so that would be O(n*m)
  * 
  * Space Complexity: O(n) beacause firstArray.filter() creates an array of at most n items and Set stores at most n items, 
  * so the overall complexity would be O(n)+O(n) which we consider O(n) as they grow linerly not exponentially
@@ -27,8 +27,8 @@ export const findCommonItems = (firstArray, secondArray) => {
       seen.add(item);
     }
   }
-
-  return [...seen]; //)(k)
+ 
+  return [...seen]; //)(k) this creates an array from seen which contains only the common items
 };
 
-// this solution is more optimised as it reduces Time Complexity. The time complexity of the inital function was exponential but now it's linear
+// this solution is more optimised as it reduces Time Complexity. The time complexity of the inital function was quadraticO(n*m) but now it's linearO(n+m+k)
