3ade6ee8a8230f6a92a61642b96f7e4f0c7475f3
nadika <nadikazavodovska@gmail.com>
2025-06-13 15:08:10 +0100

    Implement push_head method

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -8,3 +8,17 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+    def push_head(self, value):
+        new_node = ListNode(value)
+        new_node.next = self.head
+
+        if self.head:
+            self.head.previous = new_node
+
+        self.head = new_node
+
+        if self.tail is None:
+            self.tail = new_node
+
+        return new_node
