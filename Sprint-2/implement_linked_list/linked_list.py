a3e6c17017bef78530ff9c4fedd2329829bdb511
nadika <nadikazavodovska@gmail.com>
2025-06-13 12:46:41 +0100

    Add ListNode class and LinkedList

---
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -0,0 +1,10 @@
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.previous = None
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
