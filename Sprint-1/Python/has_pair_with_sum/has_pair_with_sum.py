9c36cab664943f512375c319f962c4b1af8582eb
Droid-An <andrei.filippov.aaf@gmail.com>
2025-10-09 14:24:07 +0100

    done task for the 1 sprint

--- Sprint-1/Python/has_pair_with_sum/has_pair_with_sum.py
+++ Sprint-1/Python/has_pair_with_sum/has_pair_with_sum.py
@@ -7,12 +7,30 @@ def has_pair_with_sum(numbers: List[Number], target_sum: Number) -> bool:
     """
     Find if there is a pair of numbers that sum to a target value.
 
-    Time Complexity:
-    Space Complexity:
-    Optimal time complexity:
+    Time Complexity: O(n^2)
+    Space Complexity: O(n). It's memory for the input array arr
+    Optimal time complexity: O(n)
     """
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == target_sum:
-                return True
+    # nested loop result in O(n^2) complexity
+    # for i in range(len(numbers)):
+    #     for j in range(i + 1, len(numbers)):
+    #         if numbers[i] + numbers[j] == target_sum:
+    #             return True
+    # return False
+
+    # it's better to use to pointers method here
+    # first: sort the list O(n)
+    numbers = sorted(numbers)
+    # declare the left and right pointers
+    left, right = 0, len(numbers)-1
+    # and until we "squeezed" the list check if sum of the pointers is equal to target sum
+    # in worst case we need to do n-1 steps where n is length of the list, so overall complexity will be O(n)
+    while left < right:
+        currentSum = numbers[left]+numbers[right]
+        if currentSum == target_sum:
+            return True
+        if currentSum > target_sum:
+            right -= 1
+        else:
+            left+=1
     return False
