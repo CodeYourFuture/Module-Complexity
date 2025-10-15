2a23699e10271dec5238195828e0c74f89be303f
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-10-15 18:51:41 +0100

    fibonacci becomes more efficient using cache

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,17 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+#def fibonacci(n):
+#    if n <= 1:
+#       return n 
+#  return fibonacci(n - 1) + fibonacci(n - 2)
+def fibonacci(n, cache=None):   #cache dict is created only once to store results of previous calls.
+    if cache is None:
+        cache = {}
+
+    if n in cache:   #if n is already cached return it
+        return cache[n]
+
+    if n <= 1:   #If not cached, n <= 1, return base case (0 or 1)
+        cache[n] = n
+    else:
+        cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache) #Cache the computed value for future reuse
+
+    return cache[n].    #Return the cached or newly computed value.
