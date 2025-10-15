866b1762465668cbf45d0f2179177c3296564745
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-10-15 17:52:59 +0100

    implementing the same concept of double linked list to add/remove node

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -5,3 +5,33 @@ class Node:
         self.value = value
         self.previous = None
         self.next = None
+
+#next step i define helper functions to add and remove node : 
+#I use the same concept of the previous project of the linked list
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
+        self.head.next = self.tail #this is most recently used
+        self.tail.previous = self.head  #this is the least recently used
+
+    def _add_to_head(self, node):
+        node.previous = self.head
+        node.next = self.head.next
+        self.head.next.previous = node
+        self.head.next = node
+
+    def _remove_node(self, node):
+        prev = node.previous
+        nxt = node.next
+        prev.next = nxt
+        nxt.previous = prev
+
+    def _move_to_head(self, node):
+        self._remove_node(node)
+        self._add_to_head(node)
+
+    def _pop_tail(self):
+        node = self.tail.previous
+        self._remove_node(node)
+        return node
