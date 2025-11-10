eb5a674c4207619c408efdd9f5709fe9a0c7bb56
Luke Manyamazi <lukemanyamazi1@gmail.com>
2025-11-10 13:17:01 +0200

    added code for the 2 exercises

--- Sprint-2/improve_with_precomputing/common_prefix/common_prefix_test.py
+++ Sprint-2/improve_with_precomputing/common_prefix/common_prefix_test.py
@@ -36,5 +36,32 @@ class CommonPrefixTest(unittest.TestCase):
         common_prefix = find_longest_common_prefix(strings)
         self.assertRegex(common_prefix, "^hello.*$")
 
+    def test_multiple_equal_length_prefixes(self):
+        """Test when there are multiple prefixes of the same length"""
+        strings = [
+            "apple_pie",
+            "apple_cider", 
+            "banana_split",
+            "grape_bread",
+            "grape_pie"
+        ]
+        # Both "apple_" and "grape_" are length 6, either could be returned
+        result = find_longest_common_prefix(strings)
+        self.assertTrue(result in ["apple_", "grape_"])
+        self.assertEqual(len(result), 6)
+
+    def test_identical_strings(self):
+        """Test when multiple strings are identical"""
+        strings = [
+            "identical",
+            "different",
+            "identical", 
+            "identical",
+            "another"
+        ]
+        # The longest prefix is between identical strings: "identical"
+        self.assertEqual(find_longest_common_prefix(strings), "identical")
+
+
 if __name__ == "__main__":
     unittest.main()
