393891559ced8caff63b1c73485da8ebc9b07563
Gabriel <deng93003@gmail.com>
2026-01-11 12:20:02 +0000

    I removed one loop and used numbers[i + 1
    instead

--- Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
+++ Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
@@ -9,13 +9,23 @@
  * @param {number} target - Target sum to find
  * @returns {boolean} True if pair exists, false otherwise
  */
+// export function hasPairWithSum(numbers, target) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       if (numbers[i] + numbers[j] === target) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
 export function hasPairWithSum(numbers, target) {
   for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return true;
-      }
+    if (numbers[i] + numbers[i + 1] === target) {
+      return true
     }
   }
+
   return false;
 }
