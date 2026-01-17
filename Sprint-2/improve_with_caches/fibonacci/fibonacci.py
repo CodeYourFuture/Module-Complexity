9b61b9603fe145025c84744d3c769ad0cf9302c5
Fatma <fatma.arslantas@outlook.com>
2026-01-17 19:28:40 +0000

    Optimize fibonacci: add dictionary cache to speed up recursion

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,17 @@
+# Create a dictionary to use as our "Notepad" (Cache)
+memo = {}
+
 def fibonacci(n):
+    # Check if we already calculated this number
+    if n in memo:
+        return memo[n]
+
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    result = fibonacci(n - 1) + fibonacci(n - 2)
+
+    # Save the result in our Notepad before returning
+    memo[n] = result
+    
+    return result
