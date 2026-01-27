3973c578e30a6d0af6669a1607660089dfbdf850
Droid-An <andrei.filippov.aaf@gmail.com>
2026-01-27 14:40:54 +0000

    Refactor LinkedList methods for simplicity

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -5,10 +5,9 @@ class LinkedList:
 
     def push_head(self, element):
         node = Node(element)
-        if self.head and self.tail:
+        if self.head:
             node.next = self.head
-            self.head.previous = node
-            self.head = node
+            self.head.previous = self.head = node
         elif not self.head and not self.tail:
             self.head = node
             self.tail = node
@@ -18,29 +17,27 @@ class LinkedList:
         node = self.tail
         if self.tail is not self.head:
             self.tail = node.previous
-            node.previous.next = None
-            node.previous = None
+            node.previous.next = node.previous = None
         else:
             self.head = None
             self.tail = None
         return node.value
 
     def remove(self, node):
-        if self.head != node and self.tail != node:
-            node.previous.next = node.next
-            node.next.previous = node.previous
-            node.previous, node.next = None, None
-        elif self.head == node and self.tail != node:
-            node.next.previous = None
-            self.head = node.next
-            node.previous, node.next = None, None
-        elif self.head != node and self.tail == node:
-            node.previous.next = None
-            self.tail = node.previous
-            node.previous, node.next = None, None
-        elif self.head == node and self.tail == node:
-            self.head = None
-            self.tail = None
+        if self.head == node:
+            if self.tail == node:
+                self.head = self.tail = None
+            else:
+                self.head = node.next
+                node.next.previous = node.previous = node.next = None
+        else:
+            if self.tail == node:
+                self.tail = node.previous
+                node.previous.next = node.previous = node.next = None
+            else:
+                node.previous.next = node.next
+                node.next.previous = node.previous
+                node.previous = node.next = None
 
 
 class Node:
