527cba8f01fb3519b5b52268e50f2fe18e4d14e4
Rihanna P <114884466+RihannaP@users.noreply.github.com>
2026-01-30 20:33:16 +0000

    Add dic cache and math check

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,12 +1,13 @@
 from typing import List
-
-
+cache = {}
 def ways_to_make_change(total: int) -> int:
     """
     Given access to coins with the values 1, 2, 5, 10, 20, 50, 100, 200, returns a count of all of the ways to make the passed total value.
 
     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
     """
+  
+    cache.clear()
     return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
 
 
@@ -14,9 +15,20 @@ def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
     """
     Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
     """
+    key = (total, tuple(coins))
+
+    if key in cache:
+        return cache[key]
+    
     if total == 0 or len(coins) == 0:
         return 0
-
+    
+    if len(coins) == 1:
+        if total % coins[0] == 0:
+            return 1
+        else:
+            return 0
+        
     ways = 0
     for coin_index in range(len(coins)):
         coin = coins[coin_index]
@@ -29,4 +41,5 @@ def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
                 intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
                 ways += intermediate
             count_of_coin += 1
+    cache[key] = ways
     return ways
