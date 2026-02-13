2692da2d80a230e3f40ad7ab710d09a4bff05f25
Luke Manyamazi <lukemanyamazi1@gmail.com>
2026-02-13 18:15:07 +0200

    Refactor LRU cache to use OrderedDict for O(1) operations

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -1,35 +1,27 @@
+from collections import OrderedDict
+
 class LruCache:
     def __init__(self, limit):
         if limit <= 0:
             raise ValueError("Limit must be greater than 0")
-        
         self.limit = limit
-        self.cache = {}
-        self.access_order = []  # most recent at the end
-        
+        self.cache = OrderedDict()
+
     def get(self, key):
-        if key in self.cache:
-            # Move to most recent position
-            self.access_order.remove(key)
-            self.access_order.append(key)
-            return self.cache[key]
-        return None
+        if key not in self.cache:
+            return None
+
+        # Mark as recently used
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def set(self, key, value):
         if key in self.cache:
-            # Update existing key
-            self.cache[key] = value
-            # Move to most recent position  
-            self.access_order.remove(key)
-            self.access_order.append(key)
-        else:
-            # New key
-            if len(self.cache) >= self.limit:
-                # Remove least recently used (first item)
-                lru_key = self.access_order[0]
-                del self.cache[lru_key]
-                self.access_order.pop(0)
             
-            # Add new key as most recent
-            self.cache[key] = value
-            self.access_order.append(key)
+            self.cache.move_to_end(key)
+
+        self.cache[key] = value
+
+        # Evict least recently used item
+        if len(self.cache) > self.limit:
+            self.cache.popitem(last=False)
