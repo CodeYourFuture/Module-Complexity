1ecc92e55783d6c709fe87b25579259ef777d14c
Sara Tahir <saratahir34@gmail.com>
2026-02-15 13:33:10 +0000

    Reafctored the code with one for loop for sum and product.

--- Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
+++ Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
@@ -1,5 +1,10 @@
 from typing import Dict, List
 
+#There are two form loops here. Each for loop has a time complexity of o(n)
+#Total complexity becomes o(2n), we drop constants in complexity so time complexity is o(n).
+# We are only using two varaible sum and product and no additional data structure are created,
+# so space complexity is constant at o(1)
+# We can refactor the code with one for loop which  will not affect the time or space complexity but is efficient in practise.
 
 def calculate_sum_and_product(input_numbers: List[int]) -> Dict[str, int]:
     """
@@ -21,11 +26,9 @@ def calculate_sum_and_product(input_numbers: List[int]) -> Dict[str, int]:
         return {"sum": 0, "product": 1}
 
     sum = 0
+    product =1 
     for current_number in input_numbers:
         sum += current_number
-
-    product = 1
-    for current_number in input_numbers:
         product *= current_number
 
     return {"sum": sum, "product": product}
