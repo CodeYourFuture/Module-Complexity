8e8fd4389a34193e9d48b9f1ee1f93a733deea14
sheida-shab <shabankary@gmail.com>
2026-02-15 19:24:00 +0000

    Implement  remove method

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -42,5 +42,28 @@ class LinkedList:
         return node_value    
     
     def remove(self,node) -> None:
-        pass
+        node_to_remove=node
+        current_head=self.head
+        current_tail=self.tail
+        if current_head==current_tail:
+            #linkedlist has only one node
+            self.head=None
+            self.tail=None
+        elif node_to_remove.next is None:
+            #Node is the last node(tail)
+            self.tail=node_to_remove.previous  
+            self.tail.next=None  
+        elif node_to_remove.previous is None:
+            #Node is the first node(head)   
+            self.head=node_to_remove.next
+            self.head.previous=None
+        else:
+            #Node is in the middle of list
+            previous_node=node_to_remove.previous        
+            next_node=node_to_remove.next
+            previous_node.next=next_node
+            next_node.previous=previous_node
+        
+        node_to_remove.next=None
+        node_to_remove.previous=None     
 
