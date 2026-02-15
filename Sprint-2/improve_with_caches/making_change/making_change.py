62e1e7170856df37d818498192f02cec27ec9864
Nataliia Volkova <volkov.nataliia@gmail.com>
2026-02-15 18:32:59 +0000

    making_change

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,5 +1,6 @@
 from typing import List
 
+cache = {}
 
 def ways_to_make_change(total: int) -> int:
     """
@@ -8,14 +9,25 @@ def ways_to_make_change(total: int) -> int:
     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
     """
     return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
+    
 
 
 def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
+    key = (total, tuple(coins))
+    if key in cache:
+        return cache[key]
     """
     Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
     """
     if total == 0 or len(coins) == 0:
-        return 0
+        result = 0
+        cache[key] = result
+        return result
+
+    if total == 1 or len(coins) == 1:
+        result = 1
+        cache[key] = result
+        return result
 
     ways = 0
     for coin_index in range(len(coins)):
@@ -29,4 +41,6 @@ def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
                 intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
                 ways += intermediate
             count_of_coin += 1
+
+    cache[key] = ways
     return ways
