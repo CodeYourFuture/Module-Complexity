cf4539291254cb80c5f1369d42dd14046db17434
sheida-shab <shabankary@gmail.com>
2026-02-18 14:00:18 +0000

    Precompute lowercase letters to speed up count_letters

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -3,10 +3,13 @@ def count_letters(s: str) -> int:
     count_letters returns the number of letters which only occur in upper case in the passed string.
     """
     only_upper = set()
+
+    # Precompute all lowercase letters in the string for faster lookup
+    lower_case_set={ch for ch in s if ch.islower() }
+
     for letter in s:
-        if is_upper_case(letter):
-            if letter.lower() not in s:
-                only_upper.add(letter)
+        if is_upper_case(letter) and letter.lower() not in lower_case_set:
+            only_upper.add(letter)
     return len(only_upper)
 
 
