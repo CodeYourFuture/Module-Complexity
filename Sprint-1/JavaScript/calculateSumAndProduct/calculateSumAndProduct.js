51ded7065fc6d8b8e49738032bf99ec78079b798
sheetalkharab <sheetalrana89@gmail.com>
2026-02-19 17:18:13 +0000

    restore sprint 1

--- Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
+++ Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
@@ -9,24 +9,24 @@
  *   "product": 30 // 2 * 3 * 5
  * }
  *
- * Time Complexity:O(2n) originall have this as using 2 separate loops
- * Space Complexity:O(1) no extra space used that rows with input
- * Optimal Time Complexity:O(n) must at least visit each number once
+ * Time Complexity:
+ * Space Complexity:
+ * Optimal Time Complexity:
  *
  * @param {Array<number>} numbers - Numbers to process
  * @returns {Object} Object containing running total and product
  */
-
-// here we are using 2 loops one for sum and other for product
-//  but we can do it only in one loop so code will be more simple and faster
-
 export function calculateSumAndProduct(numbers) {
   let sum = 0;
-  let product = 1;
   for (const num of numbers) {
     sum += num;
+  }
+
+  let product = 1;
+  for (const num of numbers) {
     product *= num;
   }
+
   return {
     sum: sum,
     product: product,
