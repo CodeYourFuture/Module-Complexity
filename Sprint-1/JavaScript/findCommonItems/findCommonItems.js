8fafe7ba7a7dfd687a8cae9ffcb8965f767c8c99
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-25 21:57:17 +0000

    Explain complexity using Big O Notation

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,9 +1,9 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity: O(n * m) - filter checks each element of firstArray and includes scans secondArray
+ * Space Complexity: O(n) - filter creates a new array where size grows as the input of matches grows. Same thing with set.
+ * Optimal Time Complexity: O(n + m) - each element must be examined at least once; using a Set allows O(1) lookups
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
