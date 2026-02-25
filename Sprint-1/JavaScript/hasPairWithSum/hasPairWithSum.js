1344256680906897a57c979c20af27e726ce5b13
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-25 22:52:18 +0000

    create set to achieve time efficieny - use it to store items we've already checked while looping

--- Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
+++ Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
@@ -9,13 +9,17 @@
  * @param {number} target - Target sum to find
  * @returns {boolean} True if pair exists, false otherwise
  */
-export function hasPairWithSum(numbers, target) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+
+// export function hasPairWithSum(numbers, target) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       if (numbers[i] + numbers[j] === target) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+let checkedNumbers = new set();
+
