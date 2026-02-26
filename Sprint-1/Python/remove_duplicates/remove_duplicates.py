6b1203edc70cf49255ea4c301bb5e040a2abe0dd
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-26 00:41:07 +0000

    create a set instead of an array to store unique items. This is to avoid scanning through each element in an array which is not efficient.

--- Sprint-1/Python/remove_duplicates/remove_duplicates.py
+++ Sprint-1/Python/remove_duplicates/remove_duplicates.py
@@ -11,15 +11,21 @@ def remove_duplicates(values: Sequence[ItemType]) -> List[ItemType]:
     Space complexity: O(n) - since there's another array to store unique items. its size grow proportionally as the number of unique inputs grow.
     Optimal time complexity:
     """
-    unique_items = []
+    # unique_items = []
 
-    for value in values:
-        is_duplicate = False
-        for existing in unique_items:
-            if value == existing:
-                is_duplicate = True
-                break
-        if not is_duplicate:
-            unique_items.append(value)
+    # for value in values:
+    #     is_duplicate = False
+    #     for existing in unique_items:
+    #         if value == existing:
+    #             is_duplicate = True
+    #             break
+    #     if not is_duplicate:
+    #         unique_items.append(value)
 
-    return unique_items
+    # return unique_items
+
+    unique_items = set()
+    
+
+
+   
