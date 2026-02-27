21c79881d5a152d21e3570af533a5e4b99e24577
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-27 07:01:58 +0000

    update set method to handle when the number of  keys inside a cache is greater than its limit and if so the least recently used key is removed

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -5,10 +5,17 @@ class LruCache:
         self.limit = limit
         self.cache = OrderedDict()
 
+
     def set(self, key, value):
         if key in self.cache:
             self.cache.move_to_end(key)
+        else:
+            if len(self.cache) >= self.limit:
+                self.cache.popitem(key)
+
         self.cache[key] = value
         
+
+
     def get(self, key):
         return self.cache[key]
