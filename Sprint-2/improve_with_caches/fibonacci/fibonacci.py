b5802e951e144c5cb71df7ecd38a8989dccc0a2a
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-27 21:59:50 +0000

    return the cache in both conditions instead, so that it can be read from

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,11 +1,11 @@
- cache = {} 
+cache = {} 
 
 def fibonacci(n):
     
     if n <= 1:
         cache[n] = n
-        return n
+        return cache
     
     result = fibonacci(n - 1) + fibonacci(n - 2)
     cache[n] = result
-    return result
+    return cache
