46c6ea324015d3105f7b7b3e59fe63960e076698
sheida-shab <shabankary@gmail.com>
2026-02-27 23:29:45 +0000

    Refactor removeDuplicates using  Set constructor directly

--- Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
+++ Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
@@ -6,19 +6,14 @@
  * Optimal Time Complexity:O(n)
  
  * The original implementation relied on nested loops to detect duplicates,
- *resulting in O(n²) time complexity, which does not scale well for large inputs.
- *The refactored solution uses a Set to perform duplicate checks in constant time.
- *This removes the need for an inner loop and reduces the overall time complexity
- *to O(n), which is optimal for this problem.
+ * resulting in O(n²) time complexity, which does not scale well for large inputs.
+ * The refactored solution uses a Set to perform duplicate checks in constant time.
+ * This eliminates the inner loop and reduces the overall time complexity to O(n),
+ * which is optimal for this problem.
  *using  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
  * @param {Array} inputSequence - Sequence to remove duplicates from
  * @returns {Array} New sequence with duplicates removed
  */
 export function removeDuplicates(inputSequence) {
-  const uniqueItemSet = new Set;
-
-  for (let i=0 ;i<inputSequence.length;i++){
-    uniqueItemSet.add(inputSequence[i]);
-  }
-   return [...uniqueItemSet];
+  return [...new Set(inputSequence)];
 }
