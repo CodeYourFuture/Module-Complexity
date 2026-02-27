769afdb38c98da8ef78eefd45ff19f4a51c8e6af
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-02-27 14:36:13 +0000

    Refactor LRU Cache to follow SRP - separate DoublyLinkedList class

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -1,4 +1,5 @@
 class Node:
+    """Represents a node in the doubly-linked list."""
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -6,48 +7,35 @@ class Node:
         self.previous = None
 
 
-class LruCache:
-    def __init__(self, limit):
-        if limit <= 0:
-            raise ValueError("Cache limit must be greater than 0")
-        
-        self.limit = limit
-        self.cache = {}  
-        self.head = None  
-        self.tail = None  
+class DoublyLinkedList:
+    """Manages insertion, removal, and reordering of nodes in a doubly-linked list."""
+    def __init__(self):
+        self.head = None
+        self.tail = None
     
-    def get(self, key):
-        if key not in self.cache:
-            return None
+    def add_to_head(self, node):
+        """Add a node to the head of the list."""
+        node.next = None
+        node.previous = None
         
-        node = self.cache[key]
-        
-        self._move_to_head(node)
-        
-        return node.value
-    
-    def set(self, key, value):
-        if key in self.cache:
-            node = self.cache[key]
-            node.value = value
-            self._move_to_head(node)
+        if self.head is None:
+            self.head = node
+            self.tail = node
         else:
-            if len(self.cache) >= self.limit:
-                self._evict_tail()
-            
-            new_node = Node(key, value)
-            self.cache[key] = new_node
-            self._add_to_head(new_node)
+            node.next = self.head
+            self.head.previous = node
+            self.head = node
     
-    def _move_to_head(self, node):
+    def move_to_head(self, node):
+        """Move an existing node to the head of the list."""
         if node == self.head:
             return
         
-        self._remove_node(node)
-        
-        self._add_to_head(node)
+        self.remove_node(node)
+        self.add_to_head(node)
     
-    def _remove_node(self, node):
+    def remove_node(self, node):
+        """Remove a node from the list."""
         if node == self.head == self.tail:
             self.head = None
             self.tail = None
@@ -64,27 +52,48 @@ class LruCache:
         node.next = None
         node.previous = None
     
-    def _add_to_head(self, node):
-        node.next = None
-        node.previous = None
+    def get_tail(self):
+        """Return the tail node (least recently used)."""
+        return self.tail
+
+
+class LruCache:
+    """Implements an LRU cache with O(1) operations."""
+    def __init__(self, limit):
+        if limit <= 0:
+            raise ValueError("Cache limit must be greater than 0")
         
-        if self.head is None:
-            self.head = node
-            self.tail = node
-        else:
-            node.next = self.head
-            self.head.previous = node
-            self.head = node
+        self.limit = limit
+        self.cache = {}  
+        self.order = DoublyLinkedList()
     
-    def _evict_tail(self):
-        if self.tail is None:
-            return
+    def get(self, key):
+        """Retrieve a value and mark it as recently used."""
+        if key not in self.cache:
+            return None
         
-        del self.cache[self.tail.key]
+        node = self.cache[key]
+        self.order.move_to_head(node)
         
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
+        return node.value
+    
+    def set(self, key, value):
+        """Store a key-value pair and evict LRU if necessary."""
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self.order.move_to_head(node)
         else:
-            self.tail = self.tail.previous
-            self.tail.next = None
+            if len(self.cache) >= self.limit:
+                self._evict_lru()
+            
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self.order.add_to_head(new_node)
+    
+    def _evict_lru(self):
+        """Remove the least recently used item."""
+        lru_node = self.order.get_tail()
+        if lru_node is not None:
+            del self.cache[lru_node.key]
+            self.order.remove_node(lru_node)
