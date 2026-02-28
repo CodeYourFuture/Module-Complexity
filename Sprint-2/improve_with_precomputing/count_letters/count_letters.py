dc47abbe47ca5f1056b8e45003c004057dcbc62a
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-02-28 21:23:08 +0000

    update the 2nd loop to check agaisnt the pre-calcualted lowercase letters set instead of s string

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -4,12 +4,10 @@ def count_letters(s: str) -> int:
         if letter.islower():
             only_lower.add(letter)
 
-
-
     only_upper = set()
     for letter in s:
         if is_upper_case(letter):
-            if letter.lower() not in s:
+            if letter.lower() not in only_lower:
                 only_upper.add(letter)
     return len(only_upper)
 
