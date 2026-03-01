facef1ee52562171206d98f0ff5e88392d367990
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2026-03-01 16:08:21 +0000

    Optimize Fibonacci and Making Change functions with caching for improved performance

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,12 @@
-def fibonacci(n):
+def fibonacci(n, cache={}): 
+    '''Adding a dictionary to store a copy of what we have already calculated, the complexity becomes linear.
+    With the cache, each value is computed once, bringing it down to O(n)
+    '''
     if n <= 1:
         return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+
+    if n in cache:
+        return cache[n]
+
+    cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
+    return cache[n]
