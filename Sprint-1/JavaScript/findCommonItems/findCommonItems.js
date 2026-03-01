5c99d88cb68c976d8ce7405941f2a895a3920056
Aida Eslami <ayda.eslami@gmail.com>
2026-03-01 16:52:11 +0000

    use Set for faster lookup

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -2,13 +2,23 @@
  * Finds common items between two arrays.
  *
  * Time Complexity:
- * Space Complexity:
+ *        ANSWER: O(n + m)
+          Because the second array is converted into a Set once, and the first array is looped through once.
+
+ * Space Complexity: 
+          ANSWER: O(n)
+          Because a Set is used to store unique common item 
+          
  * Optimal Time Complexity:
+          ANSWER: O(n + m)
+          Possible by using a Set for faster lookup
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
-  ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+export const findCommonItems = (firstArray, secondArray) => {
+  const secondSet = new Set(secondArray);
+
+  return [...new Set(firstArray.filter(item => secondSet.has(item)))];
+};
