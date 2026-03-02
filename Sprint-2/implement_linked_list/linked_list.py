9db693f114a817a9fcedfd69180ad547b3d4f700
katarzynakaz <l453r3@gmail.com>
2026-03-02 12:17:54 +0000

    fixes to linked list

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -8,32 +8,31 @@
 # `pop_tail` should remove an element from the end of the list.
 # * `remove` takes a handle from `push_head`, 
 # and removes that element from the list.
+class LinkedList:
+    def __init__(self):
+        self.our_list = []
+        self.tracker_number = 0
 
-our_list = []
-tracker_number = 0
+    def push_head(self, item_to_insert):
+        # assign so gets updated
+        id_for_this_particular_item = self.tracker_number
+        # //wrap item to unsert in {}
+        wrapped_item = {
+            "tracker": id_for_this_particular_item,
+            "inserted_item_key": item_to_insert
+        }
 
-def push_head(item_to_insert):
-# // grab tracker
-    global tracker_number
-    # //assign so gets updated ///
-    id_for_this_particular_item = tracker_number
-    # //wrap item to unsert in {}
-    wrapped_item = {
-        "tracker": id_for_this_particular_item,
-        "inserted_item_key": item_to_insert
-    }
+        our_list.insert(0, wrapped_item)
+        tracker_number +=1
+        return id_for_this_particular_item
 
-    our_list.insert(0, wrapped_item)
-    tracker_number +=1
-    return id_for_this_particular_item
-
-def remove(id_for_this_particular_item):
-    for item in our_list:
-        if item["tracker"] == id_for_this_particular_item:
-            our_list.remove(item)
-            return item
-        
-def pop_tail():
-    if len(our_list) > 0:
-        return our_list.pop()
+    def remove(id_for_this_particular_item):
+        for item in our_list:
+            if item["tracker"] == id_for_this_particular_item:
+                our_list.remove(item)
+                return item
             
+    def pop_tail():
+        if len(our_list) > 0:
+            return our_list.pop()
+                
