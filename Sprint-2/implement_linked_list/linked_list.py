9135980be298b087fada0c79e90c0a4023193c9b
RahwaZeslusHaile <rahwazeslase@gmail.com>
2026-03-02 21:22:21 +0000

    Refactor: Delegate pop_tail to remove, add type hints and docstrings

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -1,18 +1,22 @@
+from typing import Optional, Iterator
+
+
 class Node:
+
     def __init__(self, value):
         self.value = value
-        self.next = None
-        self.previous = None
+        self.next: Optional["Node"] = None
+        self.previous: Optional["Node"] = None
 
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head: Optional[Node] = None
+        self.tail: Optional[Node] = None
 
-    def push_head(self, value):
+    def push_head(self, value) -> Node:
         new_node = Node(value)
-        
+
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -20,41 +24,44 @@ class LinkedList:
             new_node.next = self.head
             self.head.previous = new_node
             self.head = new_node
-        
+
         return new_node
 
     def pop_tail(self):
-       
         if self.tail is None:
             raise IndexError("pop_tail from empty linked list")
-        
+
         value = self.tail.value
-        
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        else:
-            self.tail = self.tail.previous
-            self.tail.next = None
-        
+        self.remove(self.tail)  # Delegate actual removal to remove()
         return value
 
-    def remove(self, node):
+    def remove(self, node: Node):
         if node is None:
             raise ValueError("Cannot remove None node")
-        
+
         if node == self.head == self.tail:
             self.head = None
             self.tail = None
         elif node == self.head:
             self.head = node.next
-            self.head.previous = None
+            if self.head:
+                self.head.previous = None
         elif node == self.tail:
             self.tail = node.previous
-            self.tail.next = None
+            if self.tail:
+                self.tail.next = None
         else:
             node.previous.next = node.next
             node.next.previous = node.previous
-        
+
         node.next = None
         node.previous = None
+
+    def __iter__(self) -> Iterator:
+        current = self.head
+        while current:
+            yield current.value
+            current = current.next
+
+    def __repr__(self) -> str:
+        return f"LinkedList([{', '.join(str(v) for v in self)}])"
