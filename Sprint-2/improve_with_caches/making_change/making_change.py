08f9869936b00773fb8b4abb04a3d5b4544b645f
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-03 22:40:15 +0000

    updat the while lopp inside the helper function

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -19,14 +19,12 @@ def ways_to_make_change_helper(total: int, coin_index :int) -> int:
     ways = 0
    
     coin = coins[coin_index]
-    count_of_coin = 1
-    while coins * count_of_coin <= total:
-        total_from_coins = coins * count_of_coin
-        if total_from_coins == total:
-            ways += 1
-        else:
-            intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
-            ways += intermediate
+    count_of_coin = 0
+    while count_of_coin * count_of_coin <= total:
+        ways += ways_to_make_change_helper(
+            total - count_of_coin * coin,
+            coin_index + 1
+        )
         count_of_coin += 1
             
     cache[key] = ways
