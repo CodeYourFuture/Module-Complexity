6d42d37bcb05dd6878f5673c19a82f8d0969ec98
katarzynakaz <l453r3@gmail.com>
2026-03-03 18:04:28 +0000

    feedback implemented

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -1,11 +1,12 @@
 import time
+from collections import OrderedDict
 
 class LruCache:
     #  `LruCache(limit)` should construct 
     # an LRU cache which never stores more than `limit` entries.
     def __init__(self, user_limit):
         self.limit = user_limit
-        self.our_list = []  
+        self.our_list = OrderedDict()
         self.lookup_map = {} 
 
     # * `set(key, value)` should associate `value` with the passed `key`.
@@ -13,23 +14,24 @@ class LruCache:
         
         if key in self.lookup_map:
             old_item = self.lookup_map[key]
-            self.our_list.remove(old_item)
-                
+            self.our_list.pop(key)                
         wrapped_item = {
             "key": key,
             "value": value,
         }
         
     #add to list and map
-        self.our_list.insert(0, wrapped_item)
+        self.our_list[key] = wrapped_item
+        self.our_list.move_to_end(key, last=False)
         self.lookup_map[key] = wrapped_item
         
     #if full remove oldest timestamp so last
         if len(self.our_list) > self.limit:
-            oldest_item = self.our_list.pop() 
+            # oldest_item = self.our_list.pop() 
             
-            del self.lookup_map[oldest_item["key"]]
-
+            # del self.lookup_map[oldest_item["key"]]
+            oldest_key, oldest_item = self.our_list.popitem()
+            del self.lookup_map[oldest_key]
 
     # * `get(key)` should look-up the value previously associated with `key`.
     def get(self, key):
@@ -39,8 +41,9 @@ class LruCache:
             item = self.lookup_map[key]
             
             #move to front
-            self.our_list.remove(item)
-            self.our_list.insert(0, item)
+            # self.our_list.remove(item)
+            # self.our_list.insert(0, item)
+            self.our_list.move_to_end(key, last=False)
             
             return item["value"]
         return None
