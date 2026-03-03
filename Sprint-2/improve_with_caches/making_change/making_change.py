0e824bb681510f63b2fb3924e66568f0a258270c
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-03 22:18:43 +0000

    Replace the code line that repeatedly creates the same sub-arrays. Use a hard-coded list with all the coins so that we can refer to any using their index

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,5 +1,7 @@
 from typing import List
 
+coins = [200, 100, 50, 20, 10, 5, 2, 1]
+
 cache = {}
 
 def ways_to_make_change(total: int) -> int:
@@ -16,7 +18,6 @@ def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
 
     ways = 0
     for coin_index in range(len(coins)):
-        coin = coins[coin_index]
         count_of_coin = 1
         while coin * count_of_coin <= total:
             total_from_coins = coin * count_of_coin
