a7cdf3b0e80c17a66676142fd6812eb90436340e
HassanOHOsman <hassan.oh.osman@gmail.com>
2026-03-03 23:18:19 +0000

    Use "Big O Notation" to explain why the new implmentation is more efficient than the previous one

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -14,3 +14,14 @@ def count_letters(s: str) -> int:
 
 def is_upper_case(letter: str) -> bool:
     return letter == letter.upper()
+
+
+
+"""
+The old implementation scans through the entire string for every uppercase letter
+we check against, therefore complexity in the worst case is O(n^2). However, in the
+new implementation the lowercase letters a store beforehand in a set. This allows
+checking against every uppercase letter without scanning through the entire string.
+As a result, the complexixty time goes down to O(n) with the new implementation.
+
+"""
