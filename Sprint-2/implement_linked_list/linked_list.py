202c5826fd71dee159f523985af3f1ba4bd565f2
katarzynakaz <l453r3@gmail.com>
2026-03-04 16:46:40 +0000

    implemented requested changes

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -20,44 +20,39 @@ class LinkedList:
         self.head = None
         self.tail=None
         self.tracker_number = 0
-        self.our_list = []
-        
 
     def push_head(self, item_to_insert):
-        # assign so gets updated
-        id_for_this_particular_item = self.tracker_number
-        # //wrap item to unsert in {}
-        wrapped_item = Node(id_for_this_particular_item, item_to_insert)
+        # //wrap item to insert in {}
+        wrapped_item = Node(None, item_to_insert)
 
-        if self.head is None:
+        if not self.head:
             self.head = self.tail = wrapped_item
         else:
             wrapped_item.next = self.head
             self.head.prev = wrapped_item
             self.head = wrapped_item
 
-        self.our_list.insert(0, wrapped_item)
+        # self.our_list.insert(0, wrapped_item)
         self.tracker_number +=1
-        return id_for_this_particular_item
+        return wrapped_item
 
     def remove(self, id_for_this_particular_item):
-        for node in self.our_list:
-            if node.tracker == id_for_this_particular_item:
-                if node.prev:
-                    node.prev.next = node.next
-                else:
-                    self.head = node.next
+        node_to_remove = id_for_this_particular_item
+        
+        if node_to_remove.prev:
+            node_to_remove.prev.next = node_to_remove.next
+        else:
+            self.head = node_to_remove.next
 
-                if node.next:
-                    node.next.prev = node.prev
-                else:
-                    self.tail = node.prev
+        if node_to_remove.next:
+            node_to_remove.next.prev = node_to_remove.prev
+        else:
+            self.tail = node_to_remove.prev
 
-                self.our_list.remove(node)
-                return node.inserted_item_key
-            
+        return node_to_remove.inserted_item_key
+
     def pop_tail(self):
-        if not self.our_list:
+        if not self.tail:
             return None
 
         old_tail = self.tail
@@ -67,7 +62,5 @@ class LinkedList:
         else:
             self.head = self.tail = None
 
-        if len(self.our_list) > 0:
-            self.our_list.pop()
-            return old_tail.inserted_item_key
+        return old_tail.inserted_item_key
                 
