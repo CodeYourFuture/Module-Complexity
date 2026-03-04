238028a0aab5a1ff0e60b29669f266dc8503d77e
delmorallopez <124817272+delmorallopez@users.noreply.github.com>
2026-03-04 10:28:47 +0000

    lru cache update

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -6,63 +6,21 @@ class LruCache:
             raise ValueError("Limit must be positive")
 
         self.limit = limit
-<<<<<<< HEAD
-        self.cache = {}  # key -> node
-        self.head = None  # Most recently used
-        self.tail = None  # Least recently used
-
-    # ---------------------
-    # Internal helpers
-    # ---------------------
-
-    def _remove_node(self, node):
-        if node.previous:
-            node.previous.next = node.next
-        else:
-            self.head = node.next
-
-        if node.next:
-            node.next.previous = node.previous
-        else:
-            self.tail = node.previous
-
-        node.previous = None
-        node.next = None
-
-    def _add_to_head(self, node):
-        node.next = self.head
-        node.previous = None
-
-        if self.head:
-            self.head.previous = node
-        else:
-            self.tail = node
-
-        self.head = node
-
-    # ---------------------
-    # Public API
-    # ---------------------
-=======
         self.cache = OrderedDict()
->>>>>>> fc65c37 (lru cache update)
 
     def get(self, key):
         if key not in self.cache:
             return None
-        # Move key to the end (most recently used)
+
         value = self.cache.pop(key)
         self.cache[key] = value
         return value
 
     def set(self, key, value):
         if key in self.cache:
-            # Update existing and move to end
             self.cache.pop(key)
 
         elif len(self.cache) >= self.limit:
-            # Remove least recently used (first item)
             self.cache.popitem(last=False)
 
-        # Insert as most recently used
         self.cache[key] = value
