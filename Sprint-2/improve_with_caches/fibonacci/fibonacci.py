7cd1fdb8140df8101045b0f291793d13d6ac5395
eyuell21 <abrahameyuel4@gmail.com>
2026-03-08 20:18:02 +0000

    Refactor fibonacci function to use a global memoization dictionary

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,8 +1,10 @@
-def fibonacci(n, memo={}):
-    if n in memo:
-        return memo[n]
+_memo = {}
+
+def fibonacci(n):
+    if n in _memo:
+        return _memo[n]
     if n <= 1:
         return n
     
-    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
-    return memo[n]
+    _memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return _memo[n]
