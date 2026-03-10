62f8bf3452aecb5e443fab57bacc14af3224b399
katarzynakaz <l453r3@gmail.com>
2026-03-10 14:35:30 +0000

    renamed to previous, created saved_data and removed tracker

--- Sprint-2/implement_linked_list/linked_list.py
+++ Sprint-2/implement_linked_list/linked_list.py
@@ -9,47 +9,46 @@
 # * `remove` takes a handle from `push_head`, 
 # and removes that element from the list.
 class Node:
-    def __init__(self, tracker, inserted_item_key):
-        self.tracker = tracker
-        self.inserted_item_key = inserted_item_key
+    def __init__(self, data):
+        self.data = data
         self.next = None
-        self.prev = None
+        self.previous = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self.tail=None
-        self.tracker_number = 0
 
     def push_head(self, item_to_insert):
         # //wrap item to insert in {}
-        wrapped_item = Node(None, item_to_insert)
+        wrapped_item = Node(item_to_insert)
 
         if not self.head:
             self.head = self.tail = wrapped_item
         else:
             wrapped_item.next = self.head
-            self.head.prev = wrapped_item
+            self.head.previous = wrapped_item
             self.head = wrapped_item
 
-        # self.our_list.insert(0, wrapped_item)
-        self.tracker_number +=1
         return wrapped_item
 
     def remove(self, id_for_this_particular_item):
         node_to_remove = id_for_this_particular_item
         
-        if node_to_remove.prev:
-            node_to_remove.prev.next = node_to_remove.next
+        if node_to_remove.previous:
+            node_to_remove.previous.next = node_to_remove.next
         else:
             self.head = node_to_remove.next
 
         if node_to_remove.next:
-            node_to_remove.next.prev = node_to_remove.prev
+            node_to_remove.next.previous = node_to_remove.previous
         else:
-            self.tail = node_to_remove.prev
+            self.tail = node_to_remove.previous
 
-        return node_to_remove.inserted_item_key
+        #save data here
+        saved_data = node_to_remove.data
+        node_to_remove.next = node_to_remove.previous = None
+        return saved_data
 
     def pop_tail(self):
         if not self.tail:
