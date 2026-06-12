08c8476767826e2aa0ee6befa3b49bed7e1c91c4
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-06-13 01:36:55 +0300

    js taks+

--- Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
+++ Sprint-1/JavaScript/removeDuplicates/removeDuplicates.mjs
@@ -1,36 +1,20 @@
 /**
  * Remove duplicate values from a sequence, preserving the order of the first occurrence of each value.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity: O(N*N) quadratic
+ * Space Complexity: O(N)
+ * Optimal Time Complexity: become O(N)
  *
  * @param {Array} inputSequence - Sequence to remove duplicates from
  * @returns {Array} New sequence with duplicates removed
  */
 export function removeDuplicates(inputSequence) {
   const uniqueItems = [];
-
-  for (
-    let currentIndex = 0;
-    currentIndex < inputSequence.length;
-    currentIndex++
-  ) {
-    let isDuplicate = false;
-    for (
-      let compareIndex = 0;
-      compareIndex < uniqueItems.length;
-      compareIndex++
-    ) {
-      if (inputSequence[currentIndex] === uniqueItems[compareIndex]) {
-        isDuplicate = true;
-        break;
-      }
+  inputSequence.forEach((element) => {
+    if (!uniqueItems.includes(element)) {
+      uniqueItems.push(element);
     }
-    if (!isDuplicate) {
-      uniqueItems.push(inputSequence[currentIndex]);
-    }
-  }
+  });
 
   return uniqueItems;
 }
