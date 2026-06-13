c4c97dabe5269865ec598b56b8f6c2d157453b94
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-13 16:45:38 +0100

    javascript - find common item

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -9,6 +9,19 @@
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
-  ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+export const findCommonItems = (firstArray, secondArray) => {
+  if (!firstArray || !secondArray) return [];
+
+  // remove all duplicates
+  const firstSet = new Set(firstArray);
+  const secondSet = new Set(secondArray);
+
+  const common = [...firstSet].filter((item) => secondSet.has(item));
+
+  return common;
+};
+// a normal list can have same item many time 
+// we use new Set to remove dublicates for both arrays
+// because we cleaned firat array , the computer never check same word twice, same for second array
+// filter works with normal arrays so we spred firstSet to look like array
+// we put if statement if eaither one is empty it doesnt crash
