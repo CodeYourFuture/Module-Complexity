5b65a3c4f09e1ad27aa2f5e39061885df1de9690
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-13 16:46:59 +0100

    python - find common item

--- Sprint-1/Python/find_common_items/find_common_items.py
+++ Sprint-1/Python/find_common_items/find_common_items.py
@@ -13,9 +13,23 @@ def find_common_items(
     Space Complexity:
     Optimal time complexity:
     """
-    common_items: List[ItemType] = []
-    for i in first_sequence:
-        for j in second_sequence:
-            if i == j and i not in common_items:
-                common_items.append(i)
-    return common_items
+    if not first_sequence or not second_sequence:
+        return []
+
+    set_two = set(second_sequence)
+    set_first = set(first_sequence)
+    result_set = set()
+
+    for item in set_first:
+        if item in set_two:
+            result_set.add(item)
+
+    return list(result_set)
+
+# old one have a loop inside a loop for i and for j. If both lists have 10,000 items, that is like 100,000,000 checks
+
+"""
+    this function turns both lists into "Sets" to remove duplicates 
+     then, it checks each item to find 
+    the common elements without using slow nested loops.
+    """
