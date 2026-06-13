3b0d9bae12a5aed66863a3dffc4aa5de3c0006c3
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-13 16:47:28 +0100

    python - remove duplicate

--- Sprint-1/Python/remove_duplicates/remove_duplicates.py
+++ Sprint-1/Python/remove_duplicates/remove_duplicates.py
@@ -11,15 +11,21 @@ def remove_duplicates(values: Sequence[ItemType]) -> List[ItemType]:
     Space complexity:
     Optimal time complexity:
     """
+    seen = set()
     unique_items = []
 
     for value in values:
-        is_duplicate = False
-        for existing in unique_items:
-            if value == existing:
-                is_duplicate = True
-                break
-        if not is_duplicate:
+       
+        if value not in seen:
+            seen.add(value)      
             unique_items.append(value)
 
     return unique_items
+
+#old code hs a nested loop: for every single value in the original list, you loop through its unique_items list to see if it is already there. For a massive list of 100,000 items, this turns into billions of checks, making the program very slow
+"""
+    new function does through the list just once. It uses a Set as a 
+    quick memory notepad to check if an item is a duplicate. 
+    If it's a brand new item, it saves it to the Set and adds it to 
+    the result List, keeps everything in original order.
+    """
