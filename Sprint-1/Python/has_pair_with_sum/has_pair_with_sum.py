85cc9a08811b23353c2754ffc75edaa283fa26dd
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-13 16:47:15 +0100

    python - has pair sith sum

--- Sprint-1/Python/has_pair_with_sum/has_pair_with_sum.py
+++ Sprint-1/Python/has_pair_with_sum/has_pair_with_sum.py
@@ -11,8 +11,22 @@ def has_pair_with_sum(numbers: List[Number], target_sum: Number) -> bool:
     Space Complexity:
     Optimal time complexity:
     """
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == target_sum:
-                return True
+    seen_numbers = set()
+
+    for current_number in numbers:
+        needed_partner = target_sum - current_number
+
+        if needed_partner in seen_numbers:
+            return True
+
+        seen_numbers.add(current_number)
+
     return False
+
+#because old code have a nested loop (a loop inside a loop), itsmatching every number against every other single number in the list
+"""
+    new function go through the list just once, for each number, 
+    it finds the exact "matching partner" needed to reach the target. 
+    It saves everything it sees into a Set so it can check for that partner 
+ completely avoiding slow nested loops.
+    """
