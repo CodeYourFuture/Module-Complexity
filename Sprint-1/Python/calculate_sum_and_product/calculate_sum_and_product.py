8fea807de61c4d459a1af203db0227673b6843b3
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-13 16:46:42 +0100

    python - calculate sum and product

--- Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
+++ Sprint-1/Python/calculate_sum_and_product/calculate_sum_and_product.py
@@ -20,12 +20,15 @@ def calculate_sum_and_product(input_numbers: List[int]) -> Dict[str, int]:
     if not input_numbers:
         return {"sum": 0, "product": 1}
 
-    sum = 0
+    total_sum = 0
+    total_product = 1
     for current_number in input_numbers:
-        sum += current_number
+        total_sum += current_number
+        total_product *= current_number
 
-    product = 1
-    for current_number in input_numbers:
-        product *= current_number
+    return {"sum": total_sum, "product": total_product}
 
-    return {"sum": sum, "product": product}
+# in the old code we have two separate loops that run one after the other. If the list has 10,000 numbers, the first loop runs 10,000 times to calculate the sum, and the second loop runs 10,000 times to calculate the product
+# in the new code we use only one loop which will be doing same job by working half of the old code
+# if input_numbers is a small array you may dont see a diffirent but if it is a massive array then you will see it is much faster
+# sum is a built-in function name, it is safer to use a name like total_sum so Python doesnt get confused
