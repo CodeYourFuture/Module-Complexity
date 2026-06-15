50020cbe037d4439f1f96e6a9434b53c060e6e0e
Aida Eslami <ayda.eslami@gmail.com>
2026-06-15 13:15:16 +0100

    Refactor LRU Cache using OrderedDict

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -1,9 +1,4 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.previous = None
-        self.next = None
+from collections import OrderedDict
 
 
 class LruCache:
@@ -12,59 +7,40 @@ class LruCache:
             raise ValueError("Limit must be greater than 0")
 
         self.limit = limit
-        self.cache = {}
-        self.head = None
-        self.tail = None
-
-    def _remove(self, node):
-        if node.previous:
-            node.previous.next = node.next
-        else:
-            self.head = node.next
-
-        if node.next:
-            node.next.previous = node.previous
-        else:
-            self.tail = node.previous
-
-        node.previous = None
-        node.next = None
-
-    def _add_to_head(self, node):
-        node.next = self.head
-        node.previous = None
-
-        if self.head:
-            self.head.previous = node
-        else:
-            self.tail = node
-
-        self.head = node
+        self.cache = OrderedDict()
 
     def get(self, key):
         if key not in self.cache:
             return None
 
-        node = self.cache[key]
-        self._remove(node)
-        self._add_to_head(node)
+        value = self.cache.pop(key)
+        self.cache[key] = value
 
-        return node.value
+        return value
 
     def set(self, key, value):
         if key in self.cache:
-            node = self.cache[key]
-            node.value = value
-            self._remove(node)
-            self._add_to_head(node)
-            return
+            self.cache.pop(key)
 
-        if len(self.cache) >= self.limit:
-            lru = self.tail
-            if lru:
-                self._remove(lru)
-                del self.cache[lru.key]
+        elif len(self.cache) >= self.limit:
+            self.cache.popitem(last=False)
 
-        new_node = Node(key, value)
-        self._add_to_head(new_node)
-        self.cache[key] = new_node
+        self.cache[key] = value
+
+
+#  test
+
+cache = LruCache(3)
+
+cache.set("Aida", 1)
+cache.set("Bob", 2)
+cache.set("Clare", 3)
+
+print(cache.get("Aida")) 
+
+cache.set("David", 4)
+
+print(cache.get("Bob")) 
+print(cache.get("Clare")) 
+print(cache.get("David"))  
+
