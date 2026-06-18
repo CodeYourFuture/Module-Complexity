759b0e0f7e6b71dd4f8e79a5711379da7935e149
TzeMingHo <tzemingho@gmail.com>
2026-06-18 14:40:40 +0100

    completed fibonacci exercise

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,4 +1,9 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+def fibonacci(n, cache=None):
+    if cache == None:
+        cache={0:0, 1:1}
+    if n in cache:
+        return cache[n]
+    else:
+        value = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
+        cache[n] = value
+        return value
