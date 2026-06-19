d8e4ccaf51eff6018122da4822ea98ac3720d10a
TzeMingHo <tzemingho@gmail.com>
2026-06-19 20:56:28 +0100

    completed making_change.py

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -7,26 +7,35 @@ def ways_to_make_change(total: int) -> int:
 
     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
     """
-    return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1])
+    return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1], coin_index = 0, cache = None)
 
 
-def ways_to_make_change_helper(total: int, coins: List[int]) -> int:
+def ways_to_make_change_helper(total: int, coins: List[int], coin_index: int = 0, cache: dict = None) -> int:
     """
     Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
     """
-    if total == 0 or len(coins) == 0:
+    if cache == None:
+        cache = {}
+
+    current_key = (total, coin_index)
+    if current_key in cache:
+        return cache[current_key]
+    
+    if total == 0:
+        return 1
+
+    if total < 0 or coin_index >= len(coins):
         return 0
 
     ways = 0
-    for coin_index in range(len(coins)):
-        coin = coins[coin_index]
-        count_of_coin = 1
-        while coin * count_of_coin <= total:
-            total_from_coins = coin * count_of_coin
-            if total_from_coins == total:
-                ways += 1
-            else:
-                intermediate = ways_to_make_change_helper(total - total_from_coins, coins=coins[coin_index+1:])
-                ways += intermediate
-            count_of_coin += 1
+    
+    remaining_value = total - coins[coin_index]
+    
+    taken_the_coin = ways_to_make_change_helper(remaining_value, coins, coin_index, cache)
+
+    left_the_coin = ways_to_make_change_helper(total, coins, coin_index + 1, cache)
+
+    ways = taken_the_coin + left_the_coin
+
+    cache[current_key] = ways
     return ways
