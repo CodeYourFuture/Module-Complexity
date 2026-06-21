4ca58a6d3db6c51df2ec2b1dc04f086331afae38
TzeMingHo <tzemingho@gmail.com>
2026-06-21 19:18:37 +0100

    completed count_letters

--- Sprint-2/improve_with_precomputing/count_letters/count_letters.py
+++ Sprint-2/improve_with_precomputing/count_letters/count_letters.py
@@ -2,11 +2,14 @@ def count_letters(s: str) -> int:
     """
     count_letters returns the number of letters which only occur in upper case in the passed string.
     """
+    all_chars_set = set(s)
+    upper_set = set(letter for letter in all_chars_set if letter.isupper())
+    lower_set = set(letter for letter in all_chars_set if letter.islower())
+
     only_upper = set()
-    for letter in s:
-        if is_upper_case(letter):
-            if letter.lower() not in s:
-                only_upper.add(letter)
+    for letter in upper_set:
+        if letter.lower() not in lower_set:
+            only_upper.add(letter)
     return len(only_upper)
 
 
