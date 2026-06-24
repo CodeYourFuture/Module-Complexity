69205b29c6cbe3c7e2244e02e6a3527f209c6955
JanefrancessC <janefrancesschioma@gmail.com>
2026-06-24 18:24:32 +0100

    feat: Optimise findCommonItems function and reduce complexity

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,14 +1,21 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Initial code has this and can be refactored for optimisation
+ * Time Complexity: O(nm)
+ * Space Complexity: O(n)
+ * Optimal Time Complexity: O(n + m)
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
-  ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+// export const findCommonItems = (firstArray, secondArray) => [
+//   ...new Set(firstArray.filter((item) => secondArray.includes(item))),
+// ];
+// The initial solution had an expensive operation of .includes(item) search on the secondArray, resulting in O(nm) time complexity. The optimised code stores the secondArray in a set and allows O(1) lookup and reduce the complexity to O(n+m)
+export const findCommonItems = (firstArray, secondArray) => {
+  const secondSet = new Set(secondArray);
+
+  return [...new Set(firstArray.filter((item) => secondSet.has(item)))];
+};
