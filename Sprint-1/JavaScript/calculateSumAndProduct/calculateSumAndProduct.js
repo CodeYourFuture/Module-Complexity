410e893bcf8f3e144f2b5f3fabe9107116a4db68
JanefrancessC <janefrancesschioma@gmail.com>
2026-06-24 18:04:42 +0100

    feat: Optimise calculateSumAndProduct function

--- Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
+++ Sprint-1/JavaScript/calculateSumAndProduct/calculateSumAndProduct.js
@@ -9,21 +9,22 @@
  *   "product": 30 // 2 * 3 * 5
  * }
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity: O(n)
+ * Space Complexity:O(1)
+ * Optimal Time Complexity:O(n)
  *
  * @param {Array<number>} numbers - Numbers to process
  * @returns {Object} Object containing running total and product
  */
 export function calculateSumAndProduct(numbers) {
   let sum = 0;
+  let product = 1;
+
+  // Initial code looped twice through the array, i.e. 2 operations (n + n) = O(2n)operations => O(n). Limiting the loop to just one operation makes it n operation => O(n).
+  // The space complexity is constant as the variables do not grow when the function is called.
+
   for (const num of numbers) {
     sum += num;
-  }
-
-  let product = 1;
-  for (const num of numbers) {
     product *= num;
   }
 
