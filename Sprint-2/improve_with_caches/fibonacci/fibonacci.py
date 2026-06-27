be7cc5a6cd0a82b25c16d3861dae4dfba496c776
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-27 12:24:26 +0100

    update fibo

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,17 +1,13 @@
-def fibonacci(n):
-    if n < 0:
-        raise ValueError("Fibonacci is not defined for negative integers.")
-    if n <= 1:
-        return n
+fib_cache = {}
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
         
-    # Track the last two numbers in the sequence
-    prev2 = 0  # fibonacci(0)
-    prev1 = 1  # fibonacci(1)
+    if n in fib_cache:
+        return fib_cache[n]
     
-    # Calculate upward to n
-    for _ in range(2, n + 1):
-        current = prev1 + prev2
-        prev2 = prev1
-        prev1 = current
-        
-    return prev1
+    fib_cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    
+    return fib_cache[n]
