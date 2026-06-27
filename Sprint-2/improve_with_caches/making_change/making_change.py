59994df52f0f7264aa077dec512da5ea37a99a4f
ZabihollahNamazi <zabihnamazi@gmail.com>
2026-06-27 22:48:15 +0100

    update making change

--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,12 +1,37 @@
+from typing import List
+
+
 def ways_to_make_change(total: int) -> int:
-    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+  
+    return ways_to_make_change_helper(total, [200, 100, 50, 20, 10, 5, 2, 1], coin_index = 0, cache = None)
+
+
+def ways_to_make_change_helper(total: int, coins: List[int], coin_index: int = 0, cache: dict = None) -> int:
+    if cache == None:
+        cache = {}
+
+    current_key = (total, coin_index)
+    if current_key in cache:
+        return cache[current_key]
     
-    dp = [0] * (total + 1)
+    if total == 0:
+        return 1
     
-    dp[0] = 1
+    if coin_index == len(coins) - 1:
+        return 1 if total % coins[coin_index] == 0 else 0
+
+    if total < 0 or coin_index >= len(coins):
+        return 0
+
+    ways = 0
     
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] += dp[i - coin]
-            
-    return dp[total]
+    remaining_value = total - coins[coin_index]
+    
+    taken_the_coin = ways_to_make_change_helper(remaining_value, coins, coin_index, cache)
+
+    left_the_coin = ways_to_make_change_helper(total, coins, coin_index + 1, cache)
+
+    ways = taken_the_coin + left_the_coin
+
+    cache[current_key] = ways
+    return ways
