afcae8c5d28aa45b88add754da709ecfe18ada6e
TarasMykytiuk <tarasmykytiukw@gmail.com>
2026-06-28 23:59:39 +0100

    feedback fix 2

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,10 +1,8 @@
-cache = {}
-
-def fibonacci(n):
+def fibonacci(n, cache = {}):
     if n <= 1:
         return n
     if n in cache:
         return cache[n]
     else:
-        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
         return cache[n]
--- Sprint-2/improve_with_caches/making_change/making_change.py
+++ Sprint-2/improve_with_caches/making_change/making_change.py
@@ -1,7 +1,4 @@
 from typing import List
-cache = {}
-COINS = [200, 100, 50, 20, 10, 5, 2, 1]
-COINT_TYPES_NUM = len(COINS)
 
 def ways_to_make_change(total: int) -> int:
     """
@@ -9,32 +6,37 @@ def ways_to_make_change(total: int) -> int:
 
     For instance, there are two ways to make a value of 3: with 3x 1 coins, or with 1x 1 coin and 1x 2 coin.
     """
-    return ways_to_make_change_helper(total, 0)
+    coins = [200, 100, 50, 20, 10, 5, 2, 1]
+    return ways_to_make_change_helper(total, 0, coins)
 
 
-def ways_to_make_change_helper(total: int, coin_index: int) -> int:
+def ways_to_make_change_helper(total: int, coin_index: int, coins: List[int], cache = {}) -> int:
     """
     Helper function for ways_to_make_change to avoid exposing the coins parameter to callers.
     """
-    if (total, coin_index) in cache:
-        return cache[(total, coin_index)]
+    coins_types_num = len(coins)
+    index_total = (total, coin_index)
+    if index_total in cache:
+        return cache[index_total]
 
     if total == 0:
         return 1
 
-    if coin_index == COINT_TYPES_NUM:
+    if coin_index == coins_types_num:
         return 0
 
 
     ways = 0
-    coin = COINS[coin_index]
+    coin = coins[coin_index]
     count_of_coin = 0
     while count_of_coin * coin <= total:
         ways += ways_to_make_change_helper(
             total - count_of_coin * coin,
-            coin_index + 1
+            coin_index + 1,
+            coins,
+            cache
         )
         count_of_coin += 1
     
-    cache[(total, coin_index)] = ways
+    cache[index_total] = ways
     return ways
