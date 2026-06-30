73b83b21e5b3ebd4e2e48028a29de26044ed6b05
Jamal Laqdiem <jlaqdiem@gmail.com>
2026-06-30 22:45:31 +0100

    Refactor: optimize the js and py scripts to optimize time and space complexity.

--- Sprint-1/JavaScript/findCommonItems/findCommonItems.js
+++ Sprint-1/JavaScript/findCommonItems/findCommonItems.js
@@ -1,14 +1,21 @@
 /**
  * Finds common items between two arrays.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity:O(1)
+ * Space Complexity:O(n+m)
+ * Optimal Time Complexity:O(1)
  *
  * @param {Array} firstArray - First array to compare
  * @param {Array} secondArray - Second array to compare
  * @returns {Array} Array containing unique common items
  */
-export const findCommonItems = (firstArray, secondArray) => [
-  ...new Set(firstArray.filter((item) => secondArray.includes(item))),
-];
+export const findCommonItems = (firstArray, secondArray) => {
+  const secondArr = new Set(secondArray);
+
+  const uniqueValues = [
+    ...new Set(firstArray.filter((item) => secondArr.has(item))),
+  ];
+  return uniqueValues;
+};
+
+console.log(findCommonItems([2, 5, 7], [5, 6, 7]));
--- Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
+++ Sprint-1/JavaScript/hasPairWithSum/hasPairWithSum.js
@@ -1,21 +1,26 @@
 /**
  * Find if there is a pair of numbers that sum to a given target value.
  *
- * Time Complexity:
- * Space Complexity:
- * Optimal Time Complexity:
+ * Time Complexity:O(n)
+ * Space Complexity:O(n)
+ * Optimal Time Complexity:O(n)
  *
  * @param {Array<number>} numbers - Array of numbers to search through
  * @param {number} target - Target sum to find
  * @returns {boolean} True if pair exists, false otherwise
  */
 export function hasPairWithSum(numbers, target) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return true;
-      }
+  const seen = new Set();
+
+  for (let num of numbers) {
+    const complement = target - num;
+    console.log(complement);
+    if (seen.has(complement)) {
+      return true;
     }
+    seen.add(num);
   }
   return false;
 }
+
+console.log(hasPairWithSum([1, 2, 3], 4));
