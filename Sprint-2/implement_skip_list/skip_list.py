44dde927137b61d0ed9a6f00570e2a3ca7e060cd
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-07-14 17:13:21 +0100

    Implement skip list using node levels

--- Sprint-2/implement_skip_list/skip_list.py
+++ Sprint-2/implement_skip_list/skip_list.py
@@ -1,37 +1,74 @@
+import random
+
+MAX_LEVEL = 4
+
+
+class Node:
+    def __init__(self, value, level):
+        self.value = value
+        self.forward = [None] * level
+
+
 class SkipList:
     def __init__(self):
-        self.items = []
+        self.head = Node(None, MAX_LEVEL)
+        self.level = 1
+
+    def _random_level(self):
+        level = 1
+
+        while level < MAX_LEVEL and random.random() < 0.5:
+            level += 1
+
+        return level
 
     def insert(self, value):
-        left = 0
-        right = len(self.items)
+        update = [None] * MAX_LEVEL
+        current = self.head
 
-        while left < right:
-            mid = (left + right) // 2
+        for i in range(self.level - 1, -1, -1):
+            while (
+                current.forward[i] is not None
+                and current.forward[i].value < value
+            ):
+                current = current.forward[i]
 
-            if self.items[mid] < value:
-                left = mid + 1
-            else:
-                right = mid
+            update[i] = current
 
-        self.items.insert(left, value)
+        node_level = self._random_level()
+
+        if node_level > self.level:
+            for i in range(self.level, node_level):
+                update[i] = self.head
+
+            self.level = node_level
+
+        new_node = Node(value, node_level)
+
+        for i in range(node_level):
+            new_node.forward[i] = update[i].forward[i]
+            update[i].forward[i] = new_node
 
     def __contains__(self, value):
-        left = 0
-        right = len(self.items) - 1
+        current = self.head
 
-        while left <= right:
-            mid = (left + right) // 2
+        for i in range(self.level - 1, -1, -1):
+            while (
+                current.forward[i] is not None
+                and current.forward[i].value < value
+            ):
+                current = current.forward[i]
 
-            if self.items[mid] == value:
-                return True
+        current = current.forward[0]
 
-            if self.items[mid] < value:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        return False
+        return current is not None and current.value == value
 
     def to_list(self):
-        return list(self.items)
+        result = []
+        current = self.head.forward[0]
+
+        while current is not None:
+            result.append(current.value)
+            current = current.forward[0]
+
+        return result
