0adc4c66a086ca5de4fccf5db8f8b27b8538206f
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-07-14 18:58:30 +0100

    Refactor LRU cache implementation

--- Sprint-2/implement_lru_cache/lru_cache.py
+++ Sprint-2/implement_lru_cache/lru_cache.py
@@ -6,17 +6,12 @@ class Node:
         self.previous = None
 
 
-class LruCache:
-    def __init__(self, limit):
-        if limit <= 0:
-            raise ValueError("Limit must be greater than 0")
-
-        self.limit = limit
-        self.cache = {}
+class DoublyLinkedList:
+    def __init__(self):
         self.head = None
         self.tail = None
 
-    def _add_to_head(self, node):
+    def add_to_head(self, node):
         node.previous = None
         node.next = self.head
 
@@ -28,41 +23,45 @@ class LruCache:
         if self.tail is None:
             self.tail = node
 
-    def _move_to_head(self, node):
-        if node == self.head:
-            return
-
+    def remove(self, node):
         if node.previous is not None:
             node.previous.next = node.next
+        else:
+            self.head = node.next
 
         if node.next is not None:
             node.next.previous = node.previous
-
-        if node == self.tail:
+        else:
             self.tail = node.previous
 
+        node.next = None
         node.previous = None
-        node.next = self.head
 
-        if self.head is not None:
-            self.head.previous = node
-
-        self.head = node
-
-    def _remove_tail(self):
-        if self.tail is None:
+    def move_to_head(self, node):
+        if node == self.head:
             return
 
-        old_tail = self.tail
+        self.remove(node)
+        self.add_to_head(node)
 
-        if self.head == self.tail:
-            self.head = None
-            self.tail = None
-        else:
-            self.tail = old_tail.previous
-            self.tail.next = None
+    def remove_tail(self):
+        if self.tail is None:
+            return None
 
-        del self.cache[old_tail.key]
+        node = self.tail
+        self.remove(node)
+
+        return node
+
+
+class LruCache:
+    def __init__(self, limit):
+        if limit <= 0:
+            raise ValueError("Limit must be greater than 0")
+
+        self.limit = limit
+        self.cache = {}
+        self.items = DoublyLinkedList()
 
     def get(self, key):
         node = self.cache.get(key)
@@ -70,20 +69,24 @@ class LruCache:
         if node is None:
             return None
 
-        self._move_to_head(node)
+        self.items.move_to_head(node)
+
         return node.value
 
     def set(self, key, value):
         if key in self.cache:
             node = self.cache[key]
             node.value = value
-            self._move_to_head(node)
+            self.items.move_to_head(node)
             return
 
         node = Node(key, value)
 
         self.cache[key] = node
-        self._add_to_head(node)
+        self.items.add_to_head(node)
 
         if len(self.cache) > self.limit:
-            self._remove_tail()
+            old_node = self.items.remove_tail()
+
+            if old_node is not None:
+                del self.cache[old_node.key]
