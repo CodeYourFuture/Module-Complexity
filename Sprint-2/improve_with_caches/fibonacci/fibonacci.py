2a008a8848b89be1b1fd0f31ff160d9cfe88711a
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-07-14 19:07:13 +0100

    Remove global cache from fibonacci

--- Sprint-2/improve_with_caches/fibonacci/fibonacci.py
+++ Sprint-2/improve_with_caches/fibonacci/fibonacci.py
@@ -1,10 +1,10 @@
-cache = {0: 0, 1: 1}
+def fibonacci(n, cache=None):
+    if cache is None:
+        cache = {0: 0, 1: 1}
 
-
-def fibonacci(n):
     if n in cache:
         return cache[n]
 
-    cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    cache[n] = fibonacci(n - 1, cache) + fibonacci(n - 2, cache)
 
     return cache[n]
